7caf72cc4cd5118b58a0c01b65e1440fccd0410b
OD <contato@gabrielfontenelle.com>
2024-12-15 15:29:02 +0100

    feat: Add moving to FileState.

--- filejacket/file/state.py
+++ filejacket/file/state.py
@@ -46,6 +46,10 @@ class FileState:
     Indicate whether an object has changed or not. If true, we will consider that the current content was
     changed but not saved yet.  
     """
+    moving: bool = False
+    """
+    Indicate whether an object is schedule to being moved or copied in the current file`s filesystem or between filesystems.
+    """
     processing: bool = True
     """
     Indicate whether an object has already run its pipeline of extraction or not. If true, we will consider 
@@ -69,6 +73,6 @@ class FileState:
         """
         Method to allow dir and vars to work with the class simplifying the serialization of object.
         """
-        attributes: set = {"adding", "renaming", "changing", "processing"}
+        attributes: set = {"adding", "renaming", "changing", "processing", "moving"}
 
         return {key: getattr(self, key) for key in attributes}
